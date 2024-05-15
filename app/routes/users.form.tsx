4b6a41797ba6cd533d308d4ada049fd845e4203a
azevedorafael <rafaelsilva.fatec@gmail.com>
2024-05-14 21:42:56 -0300

    Add remix-forms to handle complex inputs

--- app/routes/users.form.tsx
+++ app/routes/users.form.tsx
@@ -1,28 +1,45 @@
-import type { User } from "@prisma/client";
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
 import { UserForm } from "~/components/UserForm";
 import { db } from "~/db.server";
+import { z } from 'zod'
+import { makeDomainFunction } from 'domain-functions'
+import { formAction } from '~/form-action.server' /* path to your custom formAction */
 
-export async function action({ request }: ActionFunctionArgs) {
-  // throw new Error("Yeah Mr. White! Yeah SCIENCE!!");
-  const data = Object.fromEntries(await request.formData());
 
-  await db.user.create({
-    data: data as unknown as User,
-  });
+const schema = z.object({
+  name: z.string().min(1, { message: "Please provide your name" }).trim(),
+  email: z
+    .string()
+    .min(1, { message: "Please provide your email" })
+    .email({ message: "Please provide a valid email" })
+    .trim(),
+  city: z.string().min(1, { message: "Please provide your city" }).trim(),
+  state: z.string().min(1, { message: "Please provide your state" }).trim(),
+});
 
-  return redirect("/users");
-}
+const mutation = makeDomainFunction(schema)(
+  async (data) => await db.user.create({ data })
+);
+
+export const action = async ({ request }: ActionFunctionArgs) =>
+  formAction({
+    request,
+    schema,
+    mutation,
+    successPath: '/users',
+  })
 
 export default function () {
-  return <UserForm />;
+  return <UserForm schema={schema}/>;
 }
 
-export function ErrorBoundary() {
+export function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <div className="bg-red-100 border border-red-500 p-12 text-red-500 font-bold text-2xl">
-      Something went wrong
+    <div className="bg-red-100 border border-red-500 p-12">
+      <span className=" text-red-500 font-bold text-2xl">
+        Something went wrong
+      </span>
+      {/* <p>{error.message}</p> */}
     </div>
   );
 }
