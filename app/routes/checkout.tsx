5fc8b3a446d77aa738e2b14af0d81b708b28faa6
azevedorafael <rafaelsilva.fatec@gmail.com>
2024-05-23 22:41:52 -0300

    Initial Integration with Stripe

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -1,7 +1,34 @@
-import { Checkout, getTotals } from "~/features/Checkout";
-import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { getSession } from "~/session.server";
+import { Checkout,checkoutSchema,createOrder, getTotals } from "~/features/Checkout";
+import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { commitSession, getSession } from "~/session.server";
 import { useLoaderData } from "@remix-run/react";
+import formsStyles from "~/styles/form.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: formsStyles }];
+}
+
+export async function action({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const products = session.get("cartProducts") ?? [];
+  const totals = getTotals({ products });
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = await createOrder({
+    ...checkoutSchema.parse(data),
+    products: JSON.stringify(products),
+    totals: JSON.stringify(totals),
+  });
+
+  session.set("orderId", order.id);
+
+  return redirect("/payment", {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
+}
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
